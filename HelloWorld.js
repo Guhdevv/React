89b5b7d389bc8ee5654bbaeeae4fcd12ae920ea4
Guhdevv <153245283+Guhdevv@users.noreply.github.com>
2025-06-18 23:55:04 -0300

    Update HelloWorld.js
    hhh

--- HelloWorld.js
+++ HelloWorld.js
@@ -3,6 +3,12 @@ ReactDOM.render(
    'h1',
    null,
    'Hello World' 
+   ),   
+    React.createElement(
+   'h2',
+   null,
+   'Aprendendo React' 
+ 
   ),
   document.getElementById('conteudo')
 );
